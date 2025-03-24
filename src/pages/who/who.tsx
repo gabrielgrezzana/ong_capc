bdf1375c91fb04866ca9c798de8ccc763a6d87f2
Gabriel Grezzana <106358234+gabrielgrezzana@users.noreply.github.com>
2025-03-24 15:32:36 -0300

    fix who iam.

--- src/pages/who/who.tsx
+++ src/pages/who/who.tsx
@@ -1,5 +1,6 @@
 import theme from "../../theme/theme";
 import Text from "../../atoms/text/text";
+import images from "../../assets";
 
 const WhoIAm = () => {
   return (
@@ -18,22 +19,32 @@ const WhoIAm = () => {
         style={{
           color: "#F77C2C",
           marginLeft: 40,
-          marginBottom: 25,
+          marginBottom: 250,
+          fontSize:45
         }}
       >
         Quem Somos
       </h1>
-      <div style={{ width: "70%", height: "60%" }}>
-        <Text
-          variant="history"
-          label="O C.A.P.C. não é uma entidade médica, investe, acredita no diagnóstico precoce,
- no resgate da autoestima, na orientação sobre os benefícios de atividades físicas, 
-boa alimentação, exames periódicos, acompanhamento médico, fortalecimento
- de vínculos familiares e o retorno ao convívio com a sociedade, por ter plena
- convicção que a superação do câncer dá-se 50% com tratamento oncológico
-acompanhemento médico  e os outros 50% é através de sua autoestima e sede
- insaciável de viver."
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ width: "60%", height: "100%",  display:"flex",textAlign:"center", alignItems:"center", justifyContent:"center", marginLeft:55, marginRight:55 }}>
+          <Text
+            variant="history"
+            label="O C.A.P.C. não é uma entidade médica, investe, acredita no diagnóstico precoce,
+                  no resgate da autoestima, na orientação sobre os benefícios de atividades físicas, 
+                  boa alimentação, exames periódicos, acompanhamento médico, fortalecimento
+                  de vínculos familiares e o retorno ao convívio com a sociedade, por ter plena
+                  convicção que a superação do câncer dá-se 50% com tratamento oncológico
+                  acompanhemento médico  e os outros 50% é através de sua autoestima e sede
+                  insaciável de viver."
+          />          
+        </div>
+        <div style={{ width: "35%", height: "100%",  display:"flex",textAlign:"center", alignItems:"center", justifyContent:"center" }}>
+          <img
+          src={images.LOGO_LARANJA}
+          alt="homem"
+          style={{ width: "15%"  , height: "80%", maxWidth:"300px", minWidth:"300px", maxHeight:"250px", minHeight:"250px" }}
         />
+        </div>
       </div>
     </div>
   );
